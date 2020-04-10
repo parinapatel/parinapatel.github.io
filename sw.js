e4d4bd95ae7991167a2704fe833b7e481ed29eb4
Parin Automated CI <parin_automated_CI@github.com>
2020-04-10 02:12:53 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e036520914f1074cd07e.js"
+    "url": "webpack-runtime-28bacf2be292aa1e1c30.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cffc3224fc8ba0285060f879dc3b5db2"
+    "revision": "82e7e32ee2cfed455e199f886d1041e0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
